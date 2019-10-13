6ac6dacc4a11e1fbd4075487dfd1a6a29b32eead
IAmLeftie <31018066+IAmLeftie@users.noreply.github.com>
2019-10-12 22:05:16 -0400

    v1.1.3 released
    fixed the saturation values of many foods, made jams return bottles on consuming

--- src/main/java/net/leftie/berrysmod/BerrysMod.java
+++ src/main/java/net/leftie/berrysmod/BerrysMod.java
@@ -2,6 +2,8 @@ package net.leftie.berrysmod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.leftie.berrysmod.item.GoldenJam;
+import net.leftie.berrysmod.item.Jam;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -28,19 +30,14 @@ public class BerrysMod implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("berrysmod", "berry_pie"), BERRY_PIE);
 		Registry.register(Registry.ITEM, new Identifier("berrysmod", "golden_berries"), GOLDEN_BERRIES);
 		Registry.register(Registry.ITEM, new Identifier("berrysmod", "golden_pie"), GOLDEN_PIE);
-		//Registry.register(Registry.POTION, new Identifier("berrysmod", "jam"), JAM);
-		//Registry.register(Registry.POTION, new Identifier("berrysmod", "golden_jam"), GOLDEN_JAM);
 		Registry.register(Registry.ITEM, new Identifier("berrysmod", "jam"), JAM);
 		Registry.register(Registry.ITEM, new Identifier("berrysmod", "golden_jam"), GOLDEN_JAM);
 	}
 	public static final Block BERRY_BLOCK = new Block(FabricBlockSettings.of(Material.ORGANIC).hardness(1.0F).resistance(5.0F).sounds(BlockSoundGroup.NETHER_WART).breakByHand(true).build());
-	public static final Item BERRY_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(7).saturationModifier(4.0F).build()));
+	public static final Item BERRY_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(7).saturationModifier(0.3F).build()));
 	public static final Item GOLDEN_BERRIES = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.3F).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION,60), 1.0f).build()));
-	public static final Item GOLDEN_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(4.5F).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 160), 1.0F).statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 900), 1.0F).build()));
+	public static final Item GOLDEN_PIE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(0.4F).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 160), 1.0F).statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 900), 1.0F).build()));
 	public static final Block GOLDEN_BERRY_BLOCK = new Block(FabricBlockSettings.of(Material.ORGANIC).hardness(1.2F).resistance(6.0F).sounds(BlockSoundGroup.NETHER_WART).breakByHand(true).build());
-	//TODO try to get a bottle back to the player on eating
-	//public static final Potion JAM = new Potion(new StatusEffectInstance(StatusEffects.SATURATION, 1, 1));
-	//public static final Potion GOLDEN_JAM = new Potion(new StatusEffectInstance(StatusEffects.SATURATION, 1, 1), new StatusEffectInstance(StatusEffects.REGENERATION, 80));
-	public static final Item JAM = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(4).saturationModifier(3.5F).build()).maxCount(16).group(ItemGroup.FOOD));
-	public static final Item GOLDEN_JAM = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(5).saturationModifier(4.0F).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100), 1.0F).alwaysEdible().build()).maxCount(16).group(ItemGroup.FOOD));
+	public static final Item JAM = new Jam();
+	public static final Item GOLDEN_JAM = new GoldenJam();
 }
